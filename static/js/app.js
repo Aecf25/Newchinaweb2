0add34569aff855b81acdb44c1058dc7918d6a4f
aecf <adrianchanfreitas2@hotmail.com>
2022-11-25 18:46:42 -0400

    continuacion

---
+++ static/js/app.js
@@ -0,0 +1,479 @@
+const listadeCombos1 = document.getElementById('containermenu1');
+const vercarritolayout = document.getElementById("carritomodal");
+const modalcontainer = document.getElementById("modalventana");
+const carritocestapend = document.getElementById("cestacarritopend");
+const cuerpopagina = document.querySelector("#cuerpoppal");
+const modalproductoventana = document.getElementById("modalproductoventana")
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+
+
+
+combos1.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card1"
+    content.innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p class="descproducto">${product.desc}</p>
+        <p class="precioproducto">Ref. ${product.precio}</p>
+    `;
+    listadeCombos1.append(content);
+
+    let ventanaproducto = document.createElement("button");
+    ventanaproducto.className = "ventanaproductoboton"
+    ventanaproducto.innerHTML = `
+    Agregar
+         <span></span>
+         <span></span>
+         <span></span>
+         <span></span>
+     `;
+    content.append(ventanaproducto);
+    ventanaproducto.addEventListener("click", () => { productoindividualventana(); });
+
+    const productoindividualventana = ("click", () => {
+        modalproductoventana.innerHTML = "";
+        modalproductoventana.style.display = "flex";
+        const cuadroproductoindividual = document.createElement("div");
+        cuadroproductoindividual.className = "cuadroproduct"
+        cuadroproductoindividual.style.display = "flex"
+        cuadroproductoindividual.innerHTML = `
+        <img src="${product.img}">
+        <h2> ${product.nombre}</h2>
+        <p class="descproducto">${product.desc}</p>
+        <p class="cantproducto"><p class="cantproductonum">${product.cantidad, (product.cantidad < 10) ? "0" + product.cantidad : product.cantidad}</p></p>
+        <p class="precioproducto">Ref. ${product.precio * product.cantidad}</p>`;
+        modalproductoventana.append(cuadroproductoindividual);
+
+
+        const modalbutton = document.createElement("h1");
+        modalbutton.innerText = "✖";
+        modalbutton.className = "modal-header-button";
+
+        modalbutton.addEventListener("click", () => {
+            cerrarventana();
+        });
+
+
+
+        cuadroproductoindividual.append(modalbutton);
+
+        let mascantidad = document.createElement("button");
+        mascantidad.className = "bttnplus"
+        mascantidad.innerHTML = `
+        <p>+</p>`;
+        mascantidad.addEventListener("click", () => {
+            product.cantidad++;
+            productoindividualventana();
+            savelocal()
+        });
+
+        cuadroproductoindividual.append(mascantidad);
+
+        let menoscantidad = document.createElement("button");
+        menoscantidad.className = "bttnmin"
+        menoscantidad.innerHTML = `
+        <p>_</p>`;
+        menoscantidad.addEventListener("click", () => {
+            if (product.cantidad > 1) {
+                product.cantidad--;
+                productoindividualventana();
+                savelocal();
+            };
+
+        });
+        cuadroproductoindividual.append(menoscantidad);
+
+
+        let comprar = document.createElement("button");
+        comprar.className = "botonagregar"
+        comprar.innerHTML = `
+        Agregar
+        `;
+        cuadroproductoindividual.append(comprar);
+        comprar.addEventListener("click", () => {
+            const repeat = carrito.some((repeatproduct) => repeatproduct.id === product.id);
+
+            if (repeat) {
+                carrito.map((prod) => {
+                    if (prod.id === product.id) {
+                        return prod.cantidad += product.cantidad;
+                    }
+                });
+            } else {
+                carrito.push({
+                    id: product.id,
+                    img: product.img,
+                    nombre: product.nombre,
+                    desc: product.desc,
+                    precio: product.precio,
+                    cantidad: product.cantidad,
+                });
+            }
+            console.log(carrito);
+            carritocestacontar();
+            savelocal();
+            cerrarventana();
+        });
+        cuerpopagina.style.filter = "blur(.2vw)";
+        modalproductoventana.style.opacity = "1";
+        modalproductoventana.style.transform = "translateY(5%)"
+        modalproductoventana.style.transform = "translateX(0%)"
+    });
+});
+
+
+
+const listadeCombos2 = document.getElementById('containermenu2');
+
+combos2.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card1"
+    content.innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p class="descproducto">${product.desc}</p>
+        <p class="precioproducto">Ref. ${product.precio}</p>
+    `;
+    listadeCombos2.append(content);
+
+    let ventanaproducto = document.createElement("button");
+    ventanaproducto.className = "ventanaproductoboton"
+    ventanaproducto.innerHTML = `
+    Agregar
+         <span></span>
+         <span></span>
+         <span></span>
+         <span></span>
+     `;
+    content.append(ventanaproducto);
+    ventanaproducto.addEventListener("click", () => { productoindividualventana(); });
+
+    const productoindividualventana = ("click", () => {
+        modalproductoventana.innerHTML = "";
+        modalproductoventana.style.display = "flex";
+        const cuadroproductoindividual = document.createElement("div");
+        cuadroproductoindividual.className = "cuadroproduct"
+        cuadroproductoindividual.style.display = "flex"
+        cuadroproductoindividual.innerHTML = `
+        <img src="${product.img}">
+        <h2> ${product.nombre}</h2>
+        <p class="descproducto">${product.desc}</p>
+        <p class="cantproducto"><p class="cantproductonum">${product.cantidad, (product.cantidad < 10) ? "0" + product.cantidad : product.cantidad}</p></p>
+        <p class="precioproducto">Ref. ${product.precio * product.cantidad}</p>`;
+        modalproductoventana.append(cuadroproductoindividual);
+
+
+        const modalbutton = document.createElement("h1");
+        modalbutton.innerText = "✖";
+        modalbutton.className = "modal-header-button";
+
+        modalbutton.addEventListener("click", () => {
+            cerrarventana();
+        });
+
+
+
+        cuadroproductoindividual.append(modalbutton);
+
+        let mascantidad = document.createElement("button");
+        mascantidad.className = "bttnplus"
+        mascantidad.innerHTML = `
+        <p>+</p>`;
+        mascantidad.addEventListener("click", () => {
+            product.cantidad++;
+            productoindividualventana();
+            savelocal()
+        });
+
+        cuadroproductoindividual.append(mascantidad);
+
+        let menoscantidad = document.createElement("button");
+        menoscantidad.className = "bttnmin"
+        menoscantidad.innerHTML = `
+        <p>_</p>`;
+        menoscantidad.addEventListener("click", () => {
+            if (product.cantidad > 1) {
+                product.cantidad--;
+                productoindividualventana();
+                savelocal();
+            };
+
+        });
+        cuadroproductoindividual.append(menoscantidad);
+
+
+        let comprar = document.createElement("button");
+        comprar.className = "botonagregar"
+        comprar.innerHTML = `
+        Agregar
+        `;
+        cuadroproductoindividual.append(comprar);
+        comprar.addEventListener("click", () => {
+            const repeat = carrito.some((repeatproduct) => repeatproduct.id === product.id);
+
+            if (repeat) {
+                carrito.map((prod) => {
+                    if (prod.id === product.id) {
+                        return prod.cantidad += product.cantidad;
+                    }
+                });
+            } else {
+                carrito.push({
+                    id: product.id,
+                    img: product.img,
+                    nombre: product.nombre,
+                    desc: product.desc,
+                    precio: product.precio,
+                    cantidad: product.cantidad,
+                });
+            }
+            console.log(carrito);
+            carritocestacontar();
+            savelocal();
+            cerrarventana();
+        });
+        cuerpopagina.style.filter = "blur(.2vw)";
+        modalproductoventana.style.opacity = "1";
+        modalproductoventana.style.transform = "translateY(5%)"
+        modalproductoventana.style.transform = "translateX(0%)"
+    });
+
+
+});
+
+const listadeentremeses1 = document.querySelector('#containermenu3entremes');
+
+entremeses1.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card1"
+    content.innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p class="descproducto">${product.desc}</p>
+        <p class="precioproducto">Ref. ${product.precio}</p>
+    `;
+    listadeentremeses1.append(content);
+
+
+    let ventanaproducto = document.createElement("button");
+    ventanaproducto.className = "ventanaproductoboton"
+    ventanaproducto.innerHTML = `
+    Agregar
+         <span></span>
+         <span></span>
+         <span></span>
+         <span></span>
+     `;
+    content.append(ventanaproducto);
+    ventanaproducto.addEventListener("click", () => { productoindividualventana(); });
+
+    const productoindividualventana = ("click", () => {
+        modalproductoventana.innerHTML = "";
+        modalproductoventana.style.display = "flex";
+        const cuadroproductoindividual = document.createElement("div");
+        cuadroproductoindividual.className = "cuadroproduct"
+        cuadroproductoindividual.style.display = "flex"
+        cuadroproductoindividual.innerHTML = `
+        <img src="${product.img}">
+        <h2> ${product.nombre}</h2>
+        <p class="descproducto">${product.desc}</p>
+        <p class="cantproducto"><p class="cantproductonum">${product.cantidad, (product.cantidad < 10) ? "0" + product.cantidad : product.cantidad}</p></p>
+        <p class="precioproducto">Ref. ${product.precio * product.cantidad}</p>`;
+        modalproductoventana.append(cuadroproductoindividual);
+
+
+        const modalbutton = document.createElement("h1");
+        modalbutton.innerText = "✖";
+        modalbutton.className = "modal-header-button";
+
+        modalbutton.addEventListener("click", () => {
+            cerrarventana();
+        });
+
+
+
+        cuadroproductoindividual.append(modalbutton);
+
+        let mascantidad = document.createElement("button");
+        mascantidad.className = "bttnplus"
+        mascantidad.innerHTML = `
+        <p>+</p>`;
+        mascantidad.addEventListener("click", () => {
+            product.cantidad++;
+            productoindividualventana();
+            savelocal()
+        });
+
+        cuadroproductoindividual.append(mascantidad);
+
+        let menoscantidad = document.createElement("button");
+        menoscantidad.className = "bttnmin"
+        menoscantidad.innerHTML = `
+        <p>_</p>`;
+        menoscantidad.addEventListener("click", () => {
+            if (product.cantidad > 1) {
+                product.cantidad--;
+                productoindividualventana();
+                savelocal();
+            };
+
+        });
+        cuadroproductoindividual.append(menoscantidad);
+
+
+        let comprar = document.createElement("button");
+        comprar.className = "botonagregar"
+        comprar.innerHTML = `
+        Agregar
+        `;
+        cuadroproductoindividual.append(comprar);
+        comprar.addEventListener("click", () => {
+            const repeat = carrito.some((repeatproduct) => repeatproduct.id === product.id);
+
+            if (repeat) {
+                carrito.map((prod) => {
+                    if (prod.id === product.id) {
+                        return prod.cantidad += product.cantidad;
+                    }
+                });
+            } else {
+                carrito.push({
+                    id: product.id,
+                    img: product.img,
+                    nombre: product.nombre,
+                    desc: product.desc,
+                    precio: product.precio,
+                    cantidad: product.cantidad,
+                });
+            }
+            console.log(carrito);
+            carritocestacontar();
+            savelocal();
+            cerrarventana();
+        });
+        cuerpopagina.style.filter = "blur(.2vw)";
+        modalproductoventana.style.opacity = "1";
+        modalproductoventana.style.transform = "translateY(5%)"
+        modalproductoventana.style.transform = "translateX(0%)"
+    });
+});
+
+const listadeentremeses2 = document.querySelector('#containermenu4entremes');
+
+entremeses2.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card1"
+    content.innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p class="descproducto">${product.desc}</p>
+        <p class="precioproducto">Ref. ${product.precio}</p>
+    `;
+    listadeentremeses2.append(content);
+
+
+    let ventanaproducto = document.createElement("button");
+    ventanaproducto.className = "ventanaproductoboton"
+    ventanaproducto.innerHTML = `
+    Agregar
+         <span></span>
+         <span></span>
+         <span></span>
+         <span></span>
+     `;
+    content.append(ventanaproducto);
+    ventanaproducto.addEventListener("click", () => { productoindividualventana(); });
+
+    const productoindividualventana = ("click", () => {
+        modalproductoventana.innerHTML = "";
+        modalproductoventana.style.display = "flex";
+        const cuadroproductoindividual = document.createElement("div");
+        cuadroproductoindividual.className = "cuadroproduct"
+        cuadroproductoindividual.style.display = "flex"
+        cuadroproductoindividual.innerHTML = `
+        <img src="${product.img}">
+        <h2> ${product.nombre}</h2>
+        <p class="descproducto">${product.desc}</p>
+        <p class="cantproducto"><p class="cantproductonum">${product.cantidad, (product.cantidad < 10) ? "0" + product.cantidad : product.cantidad}</p></p>
+        <p class="precioproducto">Ref. ${product.precio * product.cantidad}</p>`;
+        modalproductoventana.append(cuadroproductoindividual);
+
+
+        const modalbutton = document.createElement("h1");
+        modalbutton.innerText = "✖";
+        modalbutton.className = "modal-header-button";
+
+        modalbutton.addEventListener("click", () => {
+            cerrarventana();
+        });
+
+
+
+        cuadroproductoindividual.append(modalbutton);
+
+        let mascantidad = document.createElement("button");
+        mascantidad.className = "bttnplus"
+        mascantidad.innerHTML = `
+        <p>+</p>`;
+        mascantidad.addEventListener("click", () => {
+            product.cantidad++;
+            productoindividualventana();
+            savelocal()
+        });
+
+        cuadroproductoindividual.append(mascantidad);
+
+        let menoscantidad = document.createElement("button");
+        menoscantidad.className = "bttnmin"
+        menoscantidad.innerHTML = `
+        <p>_</p>`;
+        menoscantidad.addEventListener("click", () => {
+            if (product.cantidad > 1) {
+                product.cantidad--;
+                productoindividualventana();
+                savelocal();
+            };
+
+        });
+        cuadroproductoindividual.append(menoscantidad);
+
+
+        let comprar = document.createElement("button");
+        comprar.className = "botonagregar"
+        comprar.innerHTML = `
+        Agregar
+        `;
+        cuadroproductoindividual.append(comprar);
+        comprar.addEventListener("click", () => {
+            const repeat = carrito.some((repeatproduct) => repeatproduct.id === product.id);
+
+            if (repeat) {
+                carrito.map((prod) => {
+                    if (prod.id === product.id) {
+                        return prod.cantidad += product.cantidad;
+                    }
+                });
+            } else {
+                carrito.push({
+                    id: product.id,
+                    img: product.img,
+                    nombre: product.nombre,
+                    desc: product.desc,
+                    precio: product.precio,
+                    cantidad: product.cantidad,
+                });
+            }
+            console.log(carrito);
+            carritocestacontar();
+            savelocal();
+            cerrarventana();
+        });
+        cuerpopagina.style.filter = "blur(.2vw)";
+        modalproductoventana.style.opacity = "1";
+        modalproductoventana.style.transform = "translateY(5%)"
+        modalproductoventana.style.transform = "translateX(0%)"
+    });
+});
---
+++ static/js/products.js
@@ -0,0 +1,156 @@
+// Promociones, Extras y Populares.
+
+const Promo1 = [
+    {
+        id: 9,
+        nombre: "Combo1",
+        precio: 10.00,
+        img: "http://drive.google.com/uc?export=view&id=1lYQ6vhdHRA-W8pxycbsfGbdeiGuAHjxf",
+        desc: "Arroz Especial + Lumpia + Pollo Agridulce",
+        cantidad: 1
+    },
+    {
+        id: 10,
+        nombre: "Combo1",
+        precio: 10.50,
+        img: "http://drive.google.com/uc?export=view&id=1lYQ6vhdHRA-W8pxycbsfGbdeiGuAHjxf",
+        desc: "Arroz Especial + Lumpia + Pollo Agridulce",
+        cantidad: 1
+    },
+]
+
+
+
+
+
+
+
+// Menu Completo
+
+const combos1 = [
+    {
+        id: 1,
+        nombre: "Chow Mein Carne Mixta en plato caliente",
+        precio: 22.00,
+        img: "http://drive.google.com/uc?export=view&id=1lYQ6vhdHRA-W8pxycbsfGbdeiGuAHjxf",
+        desc: "Arroz Especial <br/>Lumpia <br/> Pollo Agridulce",
+        cantidad: 1
+    },
+    {
+        id: 2,
+        nombre: "Combo1",
+        precio: 10,
+        img: "http://drive.google.com/uc?export=view&id=1lYQ6vhdHRA-W8pxycbsfGbdeiGuAHjxf",
+        desc: "Arroz Especial + Lumpia + Pollo Agridulce",
+        cantidad: 1
+    },
+    {
+        id: 3,
+        nombre: "Combo1",
+        precio: 10,
+        img: "http://drive.google.com/uc?export=view&id=1lYQ6vhdHRA-W8pxycbsfGbdeiGuAHjxf",
+        desc: "Arroz Especial + Lumpia + Pollo Agridulce",
+        cantidad: 1
+    },
+    {
+        id: 4,
+        nombre: "Combo1",
+        precio: 10,
+        img: "http://drive.google.com/uc?export=view&id=1lYQ6vhdHRA-W8pxycbsfGbdeiGuAHjxf",
+        desc: "Arroz Especial + Lumpia + Pollo Agridulce",
+        cantidad: 1
+    },
+
+];
+
+
+const combos2 = [
+    {
+        id: 5,
+        nombre: "Combo1",
+        precio: 10,
+        img: "http://drive.google.com/uc?export=view&id=1lYQ6vhdHRA-W8pxycbsfGbdeiGuAHjxf",
+        desc: "Arroz Especial + Lumpia + Pollo Agridulce",
+        cantidad: 1
+    },
+    {
+        id: 6,
+        nombre: "Combo 1",
+        precio: 10,
+        img: "http://drive.google.com/uc?export=view&id=1lYQ6vhdHRA-W8pxycbsfGbdeiGuAHjxf",
+        desc: "Arroz Especial + Lumpia + Pollo Agridulce",
+        cantidad: 1
+    },
+    {
+        id: 7,
+        nombre: "Combo1",
+        precio: 10,
+        img: "http://drive.google.com/uc?export=view&id=1lYQ6vhdHRA-W8pxycbsfGbdeiGuAHjxf",
+        desc: "Arroz Especial + Lumpia + Pollo Agridulce",
+        cantidad: 1
+    },
+    {
+        id: 8,
+        nombre: "Combo1",
+        precio: 10,
+        img: "http://drive.google.com/uc?export=view&id=1lYQ6vhdHRA-W8pxycbsfGbdeiGuAHjxf",
+        desc: "Arroz Especial + Lumpia + Pollo Agridulce",
+        cantidad: 1
+    },
+
+
+];
+
+const entremeses1 = [
+    {
+        id: 11,
+        nombre: "Combo1",
+        precio: 10.00,
+        img: "http://drive.google.com/uc?export=view&id=1lYQ6vhdHRA-W8pxycbsfGbdeiGuAHjxf",
+        desc: "Arroz Especial + Lumpia + Pollo Agridulce",
+        cantidad: 1
+    },
+    {
+        id: 12,
+        nombre: "Combo1",
+        precio: 10.50,
+        img: "http://drive.google.com/uc?export=view&id=1lYQ6vhdHRA-W8pxycbsfGbdeiGuAHjxf",
+        desc: "Arroz Especial + Lumpia + Pollo Agridulce",
+        cantidad: 1
+    },
+]
+
+const entremeses2 = [
+    {
+        id: 13,
+        nombre: "Lumpias",
+        precio: 6.00,
+        img: "http://drive.google.com/uc?export=view&id=1lYQ6vhdHRA-W8pxycbsfGbdeiGuAHjxf",
+        desc: "Arroz Especial + Lumpia + Pollo Agridulce",
+        cantidad: 1
+    },
+    {
+        id: 14,
+        nombre: "Wanton Frito",
+        precio: 6.50,
+        img: "http://drive.google.com/uc?export=view&id=1lYQ6vhdHRA-W8pxycbsfGbdeiGuAHjxf",
+        desc: "Arroz Especial + Lumpia + Pollo Agridulce",
+        cantidad: 1
+    },
+    {
+        id: 15,
+        nombre: "Wanton Frito Relleno",
+        precio: 8.00,
+        img: "http://drive.google.com/uc?export=view&id=1lYQ6vhdHRA-W8pxycbsfGbdeiGuAHjxf",
+        desc: "Lamina delgada de masa rellena con camarón y pollo",
+        cantidad: 1
+    },
+    {
+        id: 16,
+        nombre: "Pan Chino",
+        precio: 1.50,
+        img: "http://drive.google.com/uc?export=view&id=1lYQ6vhdHRA-W8pxycbsfGbdeiGuAHjxf",
+        desc: "Pan a base de mantequilla y azucar (4unds)",
+        cantidad: 1
+    },
+]
